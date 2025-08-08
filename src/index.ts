1f049bfe2d64f4fc23f6351a4e397dba2cf7ae74
hitarth-gg <goforhitarth@gmail.com>
2025-08-08 22:58:40 +0530

    refactor: define Channel and Listener type aliases at top level

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,9 @@ interface TrackIpcEventOptions {
   method?: string;
 }
 
+type Channel = string;
+type Listener = any;
+
 let isInstalled = false;
 let isInstalledToDefaultSession = false;
 let devtronSW: Electron.ServiceWorkerMain;
@@ -28,6 +31,7 @@ function trackIpcEvent({
 }: TrackIpcEventOptions) {
   let uuid = '';
   let newArgs = args;
+
   // extract the UUID if it exists
   if (args[0] && typeof args[0] === 'object' && args[0].__uuid__devtron) {
     uuid = args[0].__uuid__devtron;
@@ -202,7 +206,7 @@ async function startServiceWorker(ses: Electron.Session, extension: Electron.Ext
 }
 
 function patchIpcMain() {
-  const listenerMap = new Map<string, Map<any, any>>(); // channel -> (originalListener -> tracked/cleaned Listener)
+  const listenerMap = new Map<Channel, Map<Listener, Listener>>(); // channel -> (originalListener -> tracked/cleaned Listener)
 
   const storeTrackedListener = (channel: string, original: any, tracked: any): void => {
     if (!listenerMap.has(channel)) {
