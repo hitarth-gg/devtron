7e7f7df3ff55dff230d694ca9ec0856353f5b258
hitarth-gg <goforhitarth@gmail.com>
2025-08-30 14:02:48 +0530

    ci: 700ms

--- spec/devtron-install-spec.ts
+++ spec/devtron-install-spec.ts
@@ -128,7 +128,7 @@ describe('Tracking IPC Events', () => {
       return 'handled';
     });
 
-    await delay(500); // If some test fails when it shouldn't, try increasing this delay
+    await delay(700); // If some test fails when it shouldn't, try increasing this delay
     registerDevtronIpc();
 
     mainWindow.webContents.send('test-renderer-on', 'arg1', 'arg2');
